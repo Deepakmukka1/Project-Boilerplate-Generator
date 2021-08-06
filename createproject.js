46c66471837c684ca4d83c94a3057e0f33ba2e55
Mukka Deepak <ramu92013@gmail.com>
2021-08-06 11:56:52 +0530

    🔨 updated code

--- createproject.js
+++ createproject.js
@@ -1,21 +1,19 @@
 #!/usr/bin/env node
-const fs=require('fs');
-const name=process.argv[2]||'Project'
+const fs = require("fs");
+const chalk = require("chalk");
+const name = process.argv[2] || "Project";
 
 fs.mkdir(name, { recursive: true }, (err) => {
-    
-    if (err)
-    {
-        console.log("Something Went Wrong !!")
-        throw err;
-    } 
-  });
-  fs.appendFile(`${name}/app.js`,`console.log("Hello world")`,function (err){
-    if (err) throw err;
-  
-  
-  });
-    fs.appendFile(`${name}/index.html`,`<!DOCTYPE html>
+  if (err) {
+    console.log(chalk.red("Something Went Wrong !!"));
+  }
+});
+fs.appendFile(`${name}/app.js`, `console.log("Hello world")`, function (err) {
+  if (err) console.log(chalk.red("Unable to create app.js file"));
+});
+fs.appendFile(
+  `${name}/index.html`,
+  `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -26,18 +24,23 @@ fs.mkdir(name, { recursive: true }, (err) => {
     <title>Project</title>
 </head>
 <body>
-    
+<h2>Hello World ! </h2>  
 </body>
-</html>`,function (err){
-  if (err) throw err;
-}
-    );
- 
-    fs.appendFile(`${name}/style.css`,`*{
+</html>`,
+  function (err) {
+    if (err) console.log(chalk.red("Unable to create index.html file"));
+  }
+);
+
+fs.appendFile(
+  `${name}/style.css`,
+  `*{
       margin:0;
       padding:0;
-    }`,function (err){
-      if (err) throw err;
-    });
- 
-  console.log(`The folder ${name} is successfully created`);
+    }`,
+  function (err) {
+    console.log(chalk.red("Unable to create style.css file"));
+  }
+);
+
+console.log(chalk.green(`The project ${name} is successfully created 🎉 `));
